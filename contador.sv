07ef80e3e34351ff88eeaec487585f705a233f92
Zamo98 <80297447+Zamo98@users.noreply.github.com>
2024-02-16 11:12:23 -0600

    Add files via upload
    contador N bits y test bench

---
+++ contador.sv
@@ -0,0 +1,15 @@
+module contador #(parameter N = 6) (
+    input wire clk,
+    input wire reset,
+    output reg [N-1:0] cont 
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        cont <= 0;
+    end else begin
+        cont <= cont + 1;
+    end
+end
+
+endmodule
