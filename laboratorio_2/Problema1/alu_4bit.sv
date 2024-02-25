7e2e6f39cc42dd2a4e365257ff96ecff09792629
Alerodcas <alerodriguezc2002@gmail.com>
2024-02-24 23:33:39 -0600

    Codigo 7 segmentos
    Se implemento el codigo para el display de 7 segmentos, convertir de binario a decimal, obtener los digitos, y obtener los bits necesarios para el display. No funcionan caso 5 y 6 del testbench.

--- laboratorio_2/Problema1/alu_4bit.sv
+++ laboratorio_2/Problema1/alu_4bit.sv
@@ -1,24 +1,39 @@
 module alu_4bit(
-  input logic [3:0] a,
-  input logic [3:0] b,
-  input logic [1:0] op,
-  output logic [7:0] result
+    input logic [3:0] a,
+    input logic [3:0] b,
+    input logic [1:0] op,
+    input logic start,
+    output logic [6:0] s1,
+    output logic [6:0] s2,
+    output logic [6:0] s3
 );
-  wire [4:0] add_result;
-  wire [4:0] sub_result;
-  wire [7:0] mul_result;
+    logic [7:0] result;
+    wire [4:0] add_result;
+    wire [4:0] sub_result;
+    wire [7:0] mul_result;
+	 
+    wire [3:0] digit_1, digit_2, digit_3;
 
-  adder_4bit adder (a, b, add_result[4:0]);
-  subtractor_4bit subtractor (a, b, sub_result[4:0]);
-  multiplier_4bit multiplier (a, b, mul_result[7:0]);
+    adder_4bit adder (a, b, add_result[4:0]);
+    subtractor_4bit subtractor (a, b, sub_result[4:0]);
+    multiplier_4bit multiplier (a, b, mul_result[7:0]);
+	 
+	 DigitExtractor extractor(.input_number(result), .digit_1(digit_1), .digit_2(digit_2), .digit_3(digit_3));
+    segmentOutput segment1(.digit(digit_1), .seg(s1));
+    segmentOutput segment2(.digit(digit_2), .seg(s2));
+    segmentOutput segment3(.digit(digit_3), .seg(s3));
 
-  always_comb begin
-    case (op)
-      2'b00: result = add_result[4] ? add_result : {4'b0, add_result[3:0]}; // Zero-extend add_result to 8 bits only if it's 4 bits
-      2'b01: result = {4'b0, sub_result}; 
-      2'b10: result = mul_result;
-      default: result = 8'b00000000;
-    endcase
+    always_ff @(posedge start) begin
+    if (start) begin
+        case (op)
+            2'b00: result <= add_result[4] ? add_result[4:0] : {3'b0, add_result[3:0]}; // Zero-extend add_result to 8 bits only if it's 4 bits
+            2'b01: result <= {4'b0, sub_result[3:0]}; 
+            2'b10: result <= mul_result;
+            default: result <= 8'b00000000;
+        endcase
+    end else begin
+        result <= 8'b00000000;
+    end
+end
 
-  end
 endmodule
--- laboratorio_2/Problema1/alu_4bit_tb.sv
+++ laboratorio_2/Problema1/alu_4bit_tb.sv
@@ -3,60 +3,81 @@ module alu_4bit_tb;
   // Declare signals
   logic [3:0] a, b;
   logic [1:0] op;
-  logic [7:0] result;
+  logic start;
+  logic [6:0] s1,s2,s3;
   
   // Instantiate the DUT
   alu_4bit dut (
     .a(a),
     .b(b),
     .op(op),
-    .result(result)
+    .start(start),
+	 .s1(s1),
+	 .s2(s2),
+	 .s3(s3)
   );
 
 
   // Test stimulus
   initial begin
+	 
     // Test case 1: Addition
+	 start = 0;
+	 #40;
     a = 4'b0110; // 6
     b = 4'b0011; // 3
     op = 2'b00; // Add
 					// 9 = 1001
-    #40;
-	 
+	 start = 1;
+    #80;
 	 // Test case 2: Addition
+	 start = 0;
+	 #40;
     a = 4'b1100; // 12
     b = 4'b0101; // 5
     op = 2'b00; // Add
 					// 17 = 10001
-    #40;
+	 start = 1;
+    #80;
 	 
 	 // Test case 3: Subtraction
+	 start = 0;
+	 #40;
     a = 4'b0110; // 6
     b = 4'b0011; // 3
     op = 2'b01; // Subtract
 					// 3 = 11
-    #40;
+	 start = 1;
+    #80;
 	 
 	 // Test case 4: Subtraction
+	 start = 0;
+	 #40;
     a = 4'b1100; // 12
     b = 4'b0101; // 5
     op = 2'b01; // Subtract
 	 				// 7 = 111
-    #40;
+	 start = 1;
+    #80;
 	 
 	 // Test case 5: Multiplier
+	 start = 0;
+	 #40;
     a = 4'b0110; // 6
     b = 4'b0011; // 3
     op = 2'b10; // Multiply
 					// 18 = 10010
-    #40;
+    start = 1;
+    #80;
 	 
 	 // Test case 6: Multiplier
-    a = 4'b1100; // 12
-    b = 4'b0101; // 5
+	 start = 0;
+	 #40;
+    a = 4'b1111; // 12
+    b = 4'b1111; // 5
     op = 2'b10; // Multiply
 	 				// 60 = 111100
-    #40;
+	 start = 1;
  
   end
 
