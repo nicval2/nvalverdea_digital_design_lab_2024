cb9fd23c07ad22a7c766572e8b73bb417717f277
Alerodcas <alerodriguezc2002@gmail.com>
2024-03-02 20:19:48 -0600

    Reasignacion de pines
    arreglo de la asignacion de pines

--- laboratorio_2/Problema1/divider_4bit.sv
+++ laboratorio_2/Problema1/divider_4bit.sv
@@ -7,36 +7,6 @@ module divider_4bit(
   
 );
 
-  logic [3:0] temp_remainder;
-  logic [3:0] temp_quotient;
-  logic carry_out;
-  logic negative;
-
-  subtractor_4bit subtractor (
-    .a(temp_remainder),
-    .b(divisor),
-    .diff(temp_remainder),
-    .carry_out(carry_out),
-    .overflow(overflow),
-    .zero(zero),
-    .negative(negative)
-  );
-
-  // Initialize temporary variables
-  initial begin
-    temp_remainder = dividend;
-    temp_quotient = 4'b0000;
-  end
-
-  // Subtraction loop
-  always @(*) begin
-    if (temp_remainder >= divisor) begin
-      temp_quotient = temp_quotient + 1'b1;
-    end
-  end
-
-  // Assign outputs
-  assign quotient = temp_quotient;
-  assign remainder = temp_remainder;
+  
 
 endmodule
