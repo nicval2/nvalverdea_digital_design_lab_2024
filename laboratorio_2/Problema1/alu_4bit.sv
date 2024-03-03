112a0358da0490976bcc21f08c7b19c02c5454ab
Alerodcas <alerodriguezc2002@gmail.com>
2024-03-02 19:13:32 -0600

    implementacion or
    Se implementa el modulo de or y se agregan casos al testbench, funciona correctamente

--- laboratorio_2/Problema1/alu_4bit.sv
+++ laboratorio_2/Problema1/alu_4bit.sv
@@ -15,8 +15,8 @@ module alu_4bit(
 	 logic sub_CFlag, sub_VFlag, sub_ZFlag, sub_NFlag;
 	 logic mul_VFlag, mul_ZFlag;
 	 logic div_VFlag, div_ZFlag;
-	 logic and_ZFlag;
-    wire [3:0] add_result, sub_result, mul_result, div_result, and_result;
+	 logic and_ZFlag, or_ZFlag;
+    wire [3:0] add_result, sub_result, mul_result, div_result, and_result, or_result;
 	 
     wire [3:0] digit_1, digit_2;
 
@@ -25,6 +25,7 @@ module alu_4bit(
     multiplier_4bit multiplier (a, b, mul_result, mul_VFlag, mul_ZFlag);
 	 divider_4bit divider (a, b, div_result, div_VFlag, div_ZFlag);
 	 and_4bit andd (a,b, and_result, and_ZFlag);
+	 or_4bit orr (a,b, or_result, or_ZFlag);
 	 
 	 DigitExtractor extractor(.input_number(result), .digit_1(digit_1), .digit_2(digit_2));
     segmentOutput segment1(.digit(digit_1), .seg(s1));
@@ -68,6 +69,13 @@ always_comb begin
                 CFlag = 1'b0;
                 VFlag = 1'b0;
             end
+				4'b00101: begin
+                result = or_result;
+                NFlag = 1'b0;
+                ZFlag = or_ZFlag;
+                CFlag = 1'b0;
+                VFlag = 1'b0;
+            end
             default: begin
                 result = 4'b0000;
                 NFlag = 1'b0;
@@ -75,6 +83,7 @@ always_comb begin
                 CFlag = 1'b0;
                 VFlag = 1'b0;
             end
+				
         endcase
     end else begin
         result = 4'b0000;
--- laboratorio_2/Problema1/alu_4bit_tb.sv
+++ laboratorio_2/Problema1/alu_4bit_tb.sv
@@ -154,7 +154,7 @@ module alu_4bit_tb;
     assert(CFlag === 1'b0) else $error("Test case 5.1 failed: Expected CFlag to be 0");
     assert(VFlag === 1'b0) else $error("Test case 5.1 failed: Expected VFlag to be 0");
 	 
-	 // Test case 5.1: AND
+	 // Test case 5.2: AND
     start = 1;
     a = 4'b0100; // 4
     b = 4'b1011; // 11
@@ -166,6 +166,32 @@ module alu_4bit_tb;
     assert(ZFlag === 1'b1) else $error("Test case 5.2 failed: Expected ZFlag to be 1");
     assert(CFlag === 1'b0) else $error("Test case 5.2 failed: Expected CFlag to be 0");
     assert(VFlag === 1'b0) else $error("Test case 5.2 failed: Expected VFlag to be 0");
+	 
+	 // Test case 6.1: OR
+    start = 1;
+    a = 4'b0110; // 6
+    b = 4'b0011; // 3
+    op = 4'b0101; // and
+    #100; // Wait for computation
+	 assert(s1 === 7'b1000000) else $error("Test case 6.1 failed: Expected s1 to be 1000000");
+    assert(s2 === 7'b1111000) else $error("Test case 6.1 failed: Expected s2 to be 1111000");
+    assert(NFlag === 1'b0) else $error("Test case 6.1 failed: Expected NFlag to be 0");
+    assert(ZFlag === 1'b0) else $error("Test case 6.1 failed: Expected ZFlag to be 0");
+    assert(CFlag === 1'b0) else $error("Test case 6.1 failed: Expected CFlag to be 0");
+    assert(VFlag === 1'b0) else $error("Test case 6.1 failed: Expected VFlag to be 0");
+	 
+	 // Test case 6.2: OR
+    start = 1;
+    a = 4'b0100; // 4
+    b = 4'b1011; // 11
+    op = 4'b0101; // and
+    #100; // Wait for computation
+	 assert(s1 === 7'b1111001) else $error("Test case 6.2 failed: Expected s1 to be 1111001");
+    assert(s2 === 7'b0010010) else $error("Test case 6.2 failed: Expected s2 to be 0010010");
+    assert(NFlag === 1'b0) else $error("Test case 6.2 failed: Expected NFlag to be 0");
+    assert(ZFlag === 1'b0) else $error("Test case 6.2 failed: Expected ZFlag to be 0");
+    assert(CFlag === 1'b0) else $error("Test case 6.2 failed: Expected CFlag to be 0");
+    assert(VFlag === 1'b0) else $error("Test case 6.2 failed: Expected VFlag to be 0");
 
 	 $display("Test finished");
 
