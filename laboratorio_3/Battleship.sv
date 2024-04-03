9fd306a7932137c008e63ec801565a5ba4e373c8
Alerodcas <alerodriguezc2002@gmail.com>
2024-04-03 11:23:21 -0600

    Asignacion de pines y arreglos errores

--- laboratorio_3/Battleship.sv
+++ laboratorio_3/Battleship.sv
@@ -1,5 +1,9 @@
 module Battleship(
-    input logic clk
+    input logic clk,
+	 output logic vgaclk, // 25.175 MHz VGA clock
+    output logic hsync, vsync,
+    output logic sync_b, blank_b, // To monitor & DAC
+    output logic [7:0] r, g, b // To video DAC
 );
 
     // Matrices de prueba, cambiar a inputs despues de probar
@@ -25,23 +29,22 @@ module Battleship(
         '{0, 0, 0, 0, 0}    
     };
 
-    logic [7:0] r, g, b;
 
     // Instantiate videoGen module
-    videoGen videoGen_inst(
-        .x(0), // Assuming x and y coordinates are always 0
-        .y(0),
-        .matriz_barcos(matriz_barcos),
-        .matriz_golpes(matriz_golpes),
-        .matriz_disparos(matriz_disparos),
-        .r(r),
-        .g(g),
-        .b(b)
-    );
-
-    // Clock divider for simulation purposes
-    always_ff @(posedge clk) begin
-        // Implement clock divider here if needed
-    end
+    // Instantiate the vga module
+	vga vga_inst (
+    .clk(clk),
+    .matriz_barcos(matriz_barcos),
+    .matriz_golpes(matriz_golpes),
+    .matriz_disparos(matriz_disparos),
+    .vgaclk(vgaclk),
+    .hsync(hsync),
+    .vsync(vsync),
+    .sync_b(sync_b),
+    .blank_b(blank_b),
+    .r(r),
+    .g(g),
+    .b(b)
+);
 
 endmodule
---
+++ laboratorio_3/output_files/Battleship.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 22.1std.2 Build 922 07/20/2023 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+	P ActionCode(Cfg)
+		Device PartName(5CSEMA5F31) Path("C:/Users/alero/Documents/GitHub/nvalverdea_digital_design_lab_2024/laboratorio_3/output_files/") File("Battleship.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
